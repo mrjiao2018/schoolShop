6f1224247ece801a44aa95f02583c9b7af132720
mrjiao <mrjiao2018@163.com>
2019-04-07 18:19:57 +0800

    配置logback并测试，日志策略：记录30天内DEBUG、INFO、ERROR 日志

--- src/main/java/edu/whu/iss/mrjiao/schoolShop/ctrl/superAdmin/AreaCtrl.java
+++ src/main/java/edu/whu/iss/mrjiao/schoolShop/ctrl/superAdmin/AreaCtrl.java
@@ -2,6 +2,8 @@ package edu.whu.iss.mrjiao.schoolShop.ctrl.superAdmin;
 
 import edu.whu.iss.mrjiao.schoolShop.service.AreaService;
 import edu.whu.iss.mrjiao.schoolShop.vo.Area;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,18 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")      //RequestMapping中的url路径默认都使用小写，不采用驼峰命名或者下划线分割
 public class AreaCtrl {
+    Logger logger = LoggerFactory.getLogger(AreaCtrl.class);
     @Autowired
     private AreaService areaService;
+
 
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     //自动将 Map 转换成json数据返回给前端
     @ResponseBody
     private Map<String, Object> listArea(){
+        logger.info("============ start ==============");
+        long startTime = System.currentTimeMillis();
+
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = new ArrayList<>();
         try {
@@ -34,6 +41,14 @@ public class AreaCtrl {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+
+        long endTime = System.currentTimeMillis();
+
+        //logger中一般使用info信息来记录方法的启动和结束
+        logger.info("============ end ==============");
+        //logger中加入debug信息方便做调试，一般涉及到调优，包含程序执行时间
+        logger.debug("costTime:[{}ms]", endTime-startTime);
+        logger.error("test error");
 
         return modelMap;
     }
