bd9b793d3ce844779e062482f4c48ca55dc1898a
mrjiao <mrjiao2018@163.com>
2019-05-03 19:25:31 +0800

    批量添加ProductCategory的 Dao 层完善并进行单元测试

--- src/main/java/edu/whu/iss/mrjiao/schoolShop/dao/ProductCategoryDao.java
+++ src/main/java/edu/whu/iss/mrjiao/schoolShop/dao/ProductCategoryDao.java
@@ -12,4 +12,12 @@ public interface ProductCategoryDao {
      * @return
      */
     List<ProductCategory> queryProductCategoryList(long shopId);
+
+    /**
+     * 批量新增商品类别
+     *
+     * @param productCategoryList
+     * @return
+     */
+    int batchInsertProductCategory(List<ProductCategory> productCategoryList);
 }
--- src/test/java/edu/whu/iss/mrjiao/schoolShop/dao/ProductCategoryTest.java
+++ src/test/java/edu/whu/iss/mrjiao/schoolShop/dao/ProductCategoryTest.java
@@ -5,7 +5,11 @@ import edu.whu.iss.mrjiao.schoolShop.vo.ProductCategory;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ProductCategoryTest extends BaseTest {
     @Autowired
@@ -15,6 +19,27 @@ public class ProductCategoryTest extends BaseTest {
     public void testQueryProductCategoryList(){
         long shopId = 7l;
         List<ProductCategory> productCategories = productCategoryDao.queryProductCategoryList(shopId);
+    }
 
+    @Test
+    public void testBatchInsertProductCategory(){
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setProductCategoryName("商品类别1");
+        productCategory.setPriority(1);
+        productCategory.setCreateTime(new Date());
+        productCategory.setLastEditTime(new Date());
+        productCategory.setShopId(7L);
+        ProductCategory productCategory2 = new ProductCategory();
+        productCategory2.setProductCategoryName("商品类别2");
+        productCategory2.setPriority(2);
+        productCategory2.setCreateTime(new Date());
+        productCategory2.setLastEditTime(new Date());
+        productCategory2.setShopId(7L);
+        List<ProductCategory> productCategoryList = new ArrayList<ProductCategory>();
+        productCategoryList.add(productCategory);
+        productCategoryList.add(productCategory2);
+        int effectedNum = productCategoryDao
+                .batchInsertProductCategory(productCategoryList);
+        assertEquals(2, effectedNum);
     }
 }
